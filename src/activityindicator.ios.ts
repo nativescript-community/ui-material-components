7551a95eec0def968506a16f88f203e56ee54b37
Martin Guillon <martin.guillon@akylas.fr>
2018-11-11 16:05:14 +0100

    autosize like android

--- src/activityindicator.ios.ts
+++ src/activityindicator.ios.ts
@@ -1,30 +1,57 @@
-import { ActivityIndicatorBase } from './activityindicator-common';
-import { themer } from './material';
-import { colorProperty } from 'tns-core-modules/ui/styling/style-properties';
-import { Color } from 'tns-core-modules/color/color';
+import { ActivityIndicatorBase } from "./activityindicator-common"
+import { themer } from "./material"
+import { colorProperty } from "tns-core-modules/ui/styling/style-properties"
+import { Color } from "tns-core-modules/color/color"
+import { screen } from "tns-core-modules/platform/platform"
 
-
+declare module "tns-core-modules/ui/core/view" {
+    interface View {
+        _onSizeChanged()
+    }
+}
 export class ActivityIndicator extends ActivityIndicatorBase {
-    nativeViewProtected: MDCActivityIndicator;
+    nativeViewProtected: MDCActivityIndicator
+    autoSize = true
 
     get ios(): MDCActivityIndicator {
-        return this.nativeViewProtected;
+        return this.nativeViewProtected
     }
     public createNativeView() {
-
-        const view =  MDCActivityIndicator.alloc().init();
-        const colorScheme = this.colorThemer || themer.getAppColorScheme();
+        const view = MDCActivityIndicator.alloc().init()
+        const colorScheme = this.colorThemer || themer.getAppColorScheme()
         if (colorScheme) {
-            MDCActivityIndicatorColorThemer.applySemanticColorSchemeToActivityIndicator(colorScheme, view);
+            MDCActivityIndicatorColorThemer.applySemanticColorSchemeToActivityIndicator(
+                colorScheme,
+                view
+            )
         }
-        return view;
+        return view
     }
-    colorThemer:MDCSemanticColorScheme
+    colorThemer: MDCSemanticColorScheme
     getColorThemer() {
         if (!this.colorThemer) {
-            this.colorThemer = MDCSemanticColorScheme.alloc().init();
+            this.colorThemer = MDCSemanticColorScheme.alloc().init()
         }
-        return this.colorThemer;
+        return this.colorThemer
+    }
+
+    _onSizeChanged(): void {
+        if (this.autoSize) {
+            this.updateStrokeRadius(
+                this.getMeasuredWidth(),
+                this.getMeasuredHeight()
+            )
+        }
+        super._onSizeChanged()
+    }
+
+    private updateStrokeRadius(width: number, height: number) {
+        const min = Math.min(width, height)
+        const strokeWidth = (0.9 * min) / 20
+        const scale = screen.mainScreen.scale
+        const radius = min / 2 - 8 * scale
+        this.nativeViewProtected.strokeWidth = strokeWidth
+        this.nativeViewProtected.radius = radius / scale
     }
 
     // public startAnimating() {
@@ -35,10 +62,14 @@ export class ActivityIndicator extends ActivityIndicatorBase {
     // }
 
     [colorProperty.getDefault](): UIColor {
-        return null;
+        return null
     }
     [colorProperty.setNative](value: UIColor | Color) {
-        this.getColorThemer().primaryColor =  value instanceof Color ? value.ios : value;
-        MDCActivityIndicatorColorThemer.applySemanticColorSchemeToActivityIndicator(this.getColorThemer(), this.nativeViewProtected);
+        this.getColorThemer().primaryColor =
+            value instanceof Color ? value.ios : value
+        MDCActivityIndicatorColorThemer.applySemanticColorSchemeToActivityIndicator(
+            this.getColorThemer(),
+            this.nativeViewProtected
+        )
     }
 }
