b2007adb67ea75fddd66d6f491d291de5a8c7ccc
Martin Guillon <martin.guillon@akylas.fr>
2018-12-26 17:05:07 +0100

    textfield angular

---
+++ src/textfield/angular/material-textfield.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { TextValueAccessor } from './textvalue-accessor';
+export const FORMS_DIRECTIVES = [TextValueAccessor];
+
+// Uncomment and add to NgModule imports if you need to use two-way binding
+// import { NativeScriptFormsModule } from "nativescript-angular/forms";
+
+// import { registerElement } from 'nativescript-angular/element-registry';
+
+// registerElement('MDTextField', () => require('../textfield').TextField);
+
+@NgModule({
+    imports: [],
+    declarations: [...FORMS_DIRECTIVES],
+    exports: [...FORMS_DIRECTIVES],
+    providers: []
+})
+export class MaterialTextfieldModule {}
