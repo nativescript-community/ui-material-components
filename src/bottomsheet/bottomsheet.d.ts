4d086ed39491e55f7cd11f64e972fa7677bbdc62
Martin Guillon <martin.guillon@akylas.fr>
2018-12-26 17:05:15 +0100

    optional

--- src/bottomsheet/bottomsheet.d.ts
+++ src/bottomsheet/bottomsheet.d.ts
@@ -2,7 +2,7 @@ import { ViewBase } from "tns-core-modules/ui/page/page"
 
 export interface BottomSheetOptions {
     view: string | ViewBase // View instance to be shown in bottom sheet. Or the name of the module to load starting from the application root.
-    context: any //Any context you want to pass to the view shown in bottom sheet. This same context will be available in the arguments of the shownInBottomSheet event handler.
+    context?: any //Any context you want to pass to the view shown in bottom sheet. This same context will be available in the arguments of the shownInBottomSheet event handler.
     animated?: boolean // An optional parameter specifying whether to show the sheet view with animation.
     dismissOnBackgroundTap?: boolean // An optional parameter specifying whether to dismiss the sheet when clicking on background.
     closeCallback?: Function //  A function that will be called when the view is closed. Any arguments provided when calling shownInBottomSheet.closeCallback will be available here.
