e53e94c8b581a60ff3bc1164bc70c7ca50cd9442
Manuel Saelices <msaelices@gmail.com>
2019-01-21 11:51:28 +0100

    Add TextField sample to the Vue demo app.

--- demo-vue/app/examples/index.ts
+++ demo-vue/app/examples/index.ts
@@ -1,11 +1,13 @@
 import ActivityIndicators from './ActivityIndicators';
 import Buttons from './Buttons';
 import CardViews from './CardViews';
+import TextFields from './TextFields';
 
 export const getExamples = () => {
   return [
     Buttons,
     CardViews,
     ActivityIndicators,
+    TextFields,
   ];
 };
--- demo-vue/app/main.ts
+++ demo-vue/app/main.ts
@@ -3,6 +3,7 @@ import ActivityIndicatorPlugin from 'nativescript-material-activityindicator/vue
 import ButtonPlugin from 'nativescript-material-button/vue';
 import CardViewPlugin from 'nativescript-material-cardview/vue';
 import FloatingActionButtonPlugin from 'nativescript-material-floatingactionbutton/vue';
+import TextFieldPlugin from 'nativescript-material-textfield/vue';
 import { install as installBottomSheet } from 'nativescript-material-bottomsheet';
 
 installBottomSheet();
@@ -11,6 +12,7 @@ Vue.use(ActivityIndicatorPlugin);
 Vue.use(ButtonPlugin);
 Vue.use(CardViewPlugin);
 Vue.use(FloatingActionButtonPlugin);
+Vue.use(TextFieldPlugin);
 
 import { getExamples } from './examples';
 import * as views from './views';
