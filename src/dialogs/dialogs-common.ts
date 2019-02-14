3137a568505f78a8ba7de37937b0d7c0c7d1ac5c
Martin Guillon <martin.guillon@akylas.fr>
2019-02-14 15:46:43 +0100

    refactoring

--- src/dialogs/dialogs-common.ts
+++ src/dialogs/dialogs-common.ts
@@ -1,20 +1,4 @@
 import { Color } from 'tns-core-modules/ui/core/view';
-import { Page } from 'tns-core-modules/ui/page/page';
-
-declare module 'tns-core-modules/ui/dialogs' {
-    function isDialogOptions(arg): boolean;
-    function getTextFieldColor(): Color;
-    function getLabelColor(): Color;
-    function getButtonColors(): { color: Color; backgroundColor: Color };
-    function getCurrentPage(): Page;
-    const STRING: string;
-    const PROMPT: string;
-    const CONFIRM: string;
-    const ALERT: string;
-    const LOGIN: string;
-    const OK: string;
-    const CANCEL: string;
-}
 
 declare module 'tns-core-modules/ui/core/view' {
     interface View {
--- src/dialogs/dialogs.d.ts
+++ src/dialogs/dialogs.d.ts
@@ -3,8 +3,27 @@ import { Font } from 'tns-core-modules/ui/styling/font';
 import { Color } from 'tns-core-modules/color/color';
 import { TextAlignment, View } from 'tns-core-modules/ui/text-base/text-base';
 import { ImageSource } from 'tns-core-modules/image-source/image-source';
+import { Page } from 'tns-core-modules/ui/page/page';
+
+
+declare module 'tns-core-modules/ui/dialogs' {
+    function isDialogOptions(arg): boolean;
+    function getTextFieldColor(): Color;
+    function getLabelColor(): Color;
+    function getButtonColors(): { color: Color; backgroundColor: Color };
+    function getCurrentPage(): Page;
+    const STRING: string;
+    const PROMPT: string;
+    const CONFIRM: string;
+    const ALERT: string;
+    const LOGIN: string;
+    const OK: string;
+    const CANCEL: string;
+}
+
 
 export * from 'tns-core-modules/ui/dialogs';
+
 
 export interface MDCAlertControlerOptions {
     buttonFont?: Font;
