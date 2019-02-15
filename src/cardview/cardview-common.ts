68a216af0ec7810bb583caad680e12e1bf15051e
Martin Guillon <martin.guillon@akylas.fr>
2019-02-15 09:37:22 +0100

    update all cardview component

--- src/cardview/cardview-common.ts
+++ src/cardview/cardview-common.ts
@@ -1,38 +1,23 @@
-import { Color, CSSType, Property } from 'tns-core-modules/ui/core/view';
+import { Color, CSSType, Length, View } from 'tns-core-modules/ui/core/view';
 import { StackLayout } from 'tns-core-modules/ui/layouts/stack-layout';
 import { cssProperty } from 'nativescript-material-core/cssproperties';
 
 @CSSType('MDCardView')
 export abstract class CardViewBase extends StackLayout {
-    borderColor: string;
-    borderRadius: number;
-    borderWidth: number;
-    interactable: boolean;
+    // protected _borderRadius: number;
+    // protected _borderColor: Color;
+    // protected _borderWidth: number;
 
     @cssProperty elevation: number;
     @cssProperty rippleColor: Color;
+
+    // get borderRadius(): string | Length {
+    //     return this._borderRadius;
+    // }
+    // get borderWidth(): string | Length {
+    //     return this._borderWidth;
+    // }
+    // get borderColor(): Color | string {
+    //     return this._borderColor;
+    // }
 }
-
-export const borderColorProperty = new Property<CardViewBase, Color>({
-    name: 'borderColor',
-    valueConverter: value => new Color(value)
-});
-borderColorProperty.register(CardViewBase);
-
-export const borderRadiusProperty = new Property<CardViewBase, number>({
-    name: 'borderRadius',
-    valueConverter: value => +value
-});
-borderRadiusProperty.register(CardViewBase);
-
-export const borderWidthProperty = new Property<CardViewBase, number>({
-    name: 'borderWidth',
-    valueConverter: value => +value
-});
-borderWidthProperty.register(CardViewBase);
-
-export const interactableProperty = new Property<CardViewBase, boolean>({
-    name: 'interactable',
-    valueConverter: value => value === 'true'
-});
-interactableProperty.register(CardViewBase);
--- src/cardview/cardview.ios.ts
+++ src/cardview/cardview.ios.ts
@@ -1,11 +1,12 @@
-import { CardViewBase, borderRadiusProperty, borderColorProperty, borderWidthProperty, interactableProperty } from './cardview-common';
+import { CardViewBase } from './cardview-common';
 import { elevationProperty, rippleColorProperty } from 'nativescript-material-core/cssproperties';
-import { Color, Length } from 'tns-core-modules/ui/page/page';
+import { backgroundInternalProperty, Color, Length } from 'tns-core-modules/ui/page/page';
 import { getRippleColor, themer } from 'nativescript-material-core';
-import { backgroundColorProperty, backgroundInternalProperty } from 'tns-core-modules/ui/core/view/view';
+import { Background } from 'tns-core-modules/ui/styling/background';
 
 export class CardView extends CardViewBase {
     nativeViewProtected: MDCCard;
+    _backgroundColor: Color;
 
     public createNativeView() {
         const view = MDCCard.new();
@@ -13,74 +14,31 @@ export class CardView extends CardViewBase {
         if (colorScheme) {
             MDCCardsColorThemer.applySemanticColorSchemeToCard(colorScheme, view);
         }
+        // if (this._backgroundColor) {
+        //     view.backgroundColor = this._backgroundColor.ios;
+        // }
+        // if (this._borderRadius !== undefined) {
+        //     view.layer.cornerRadius = this._borderRadius;
+        // }
         return view;
     }
     _setNativeClipToBounds() {
         // this.ios.clipsToBounds = true;
     }
 
-    [backgroundColorProperty.setNative](value: Color) {
-        this.nativeView.backgroundColor = value !== undefined ? value.ios : new Color('#FFFFFF').ios;
-    }
-
-    [backgroundColorProperty.getDefault]() {
-        return this.nativeView.backgroundColor;
-    }
-
-    [backgroundInternalProperty.setNative](value) {
-        this.nativeView.backgroundColor = new Color(value.color !== undefined ? value.color + '' : '#FFFFFF').ios;
-    }
-
-    [backgroundInternalProperty.getDefault]() {
-        return this.nativeView.backgroundColor;
-    }
-
-    [borderColorProperty.setNative](value: Color) {
-        this.nativeView.setBorderColorForState(value !== undefined ? value.ios : new Color('#FFFFFF').ios, UIControlState.Normal);
-    }
-
-    [borderColorProperty.getDefault](): Color {
-        return this.nativeView.borderColorForState(UIControlState.Normal);
-    }
-
-    [borderRadiusProperty.setNative](value: number) {
-        this.nativeViewProtected.cornerRadius = value;
-    }
-
-    [borderRadiusProperty.getDefault](): number {
-        return this.nativeViewProtected.cornerRadius;
-    }
-
-    [borderWidthProperty.setNative](value: number) {
-        this.nativeView.setBorderWidthForState(value, UIControlState.Normal);
-    }
-
-    [borderWidthProperty.getDefault](): number {
-        return this.nativeView.borderWidthForState(UIControlState.Normal);
-    }
-
     [elevationProperty.setNative](value: number) {
         this.nativeViewProtected.setShadowElevationForState(value, UIControlState.Normal);
         this.nativeViewProtected.setShadowElevationForState(value * 2, UIControlState.Highlighted);
     }
-
-    [elevationProperty.getDefault]() {
-        return this.nativeView.shadowElevationForState(UIControlState.Normal);
+    [backgroundInternalProperty.setNative](value: Background) {
+        if (this.nativeViewProtected) {
+            this.nativeViewProtected.backgroundColor = value.color ? value.color.ios : null;
+            this.nativeViewProtected.setBorderWidthForState(value.borderLeftWidth, UIControlState.Normal);
+            this.nativeViewProtected.setBorderColorForState(value.borderTopColor ? value.borderTopColor.ios : null, UIControlState.Normal);
+            this.nativeViewProtected.layer.cornerRadius = value.borderTopLeftRadius;
+        }
     }
-
-    [interactableProperty.setNative](value: boolean) {
-        this.nativeView.interactable = value;
-    }
-
-    [interactableProperty.getDefault](): boolean {
-        return this.nativeView.interactable;
-    }
-
     [rippleColorProperty.setNative](color: Color) {
         this.nativeViewProtected.inkView.inkColor = getRippleColor(color);
-    }
-
-    [rippleColorProperty.getDefault]() {
-        return this.nativeViewProtected.inkView.inkColor;
     }
 }
