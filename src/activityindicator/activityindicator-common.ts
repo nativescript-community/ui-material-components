a4d4090926c51ffa789615d04bd5dc522886cd9a
Martin Guillon <martin.guillon@akylas.fr>
2019-02-15 09:38:08 +0100

    cleanup

--- src/activityindicator/activityindicator-common.ts
+++ src/activityindicator/activityindicator-common.ts
@@ -4,29 +4,19 @@ import { cssProperty } from 'nativescript-material-core/cssproperties';
 
 @CSSType('MDActivityIndicator')
 export abstract class ActivityIndicatorBase extends NSActivityIndicator {
-    // public variant: string;
-    // @cssProperty elevation: number
-    // @cssProperty rippleColor: Color | string
     public startAnimating() {
         this.busy = true;
-        // this.nativeViewProtected.startAnimating();
     }
     public stopAnimating() {
         this.busy = false;
-        // this.nativeViewProtected.stopAnimating();
     }
 
     public onMeasure(widthMeasureSpec: number, heightMeasureSpec: number): void {
-        // const view = this.nativeViewProtected;
         const width = layout.getMeasureSpecSize(widthMeasureSpec);
         const widthMode = layout.getMeasureSpecMode(widthMeasureSpec);
 
         const height = layout.getMeasureSpecSize(heightMeasureSpec);
         const heightMode = layout.getMeasureSpecMode(heightMeasureSpec);
-
-        // console.log('onMeasure', width, widthMode, height, heightMode);
-        // let nativeWidth = 0;
-        // let nativeHeight = 0;
         const finiteWidth: boolean = widthMode === layout.EXACTLY;
         const finiteHeight: boolean = heightMode === layout.EXACTLY;
         if (finiteWidth || finiteHeight) {
@@ -38,10 +28,4 @@ export abstract class ActivityIndicatorBase extends NSActivityIndicator {
             super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         }
     }
-
-    // public setMeasuredDimension(measuredWidth: number, measuredHeight: number): void {
-    //     const actual = Math.min(measuredWidth, measuredHeight)
-    //     console.log('setMeasuredDimension', measuredWidth, measuredHeight);
-    //     super.setMeasuredDimension(actual, actual);
-    // }
 }
