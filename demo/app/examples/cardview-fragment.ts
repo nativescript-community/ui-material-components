ab1a9bf46d7b455f99aa466d7b6b6a6689327998
Martin Guillon <martin.guillon@akylas.fr>
2019-02-15 10:13:24 +0100

    better cardview example

--- demo/app/examples/cardview-fragment.ts
+++
@@ -1,6 +0,0 @@
-import { EventData, View } from 'tns-core-modules/ui/core/view';
-import { ItemEventData } from 'tns-core-modules/ui/list-view/list-view';
-import 'nativescript-material-cardview'
-
-export function onTap(args: ItemEventData) {
-}
