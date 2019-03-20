db7b69db388dc37c0d3822519755b15a34e1e839
Martin Guillon <martin.guillon@akylas.fr>
2019-03-20 09:38:35 +0100

    Cardiview example

--- demo-vue/app/examples/CardViews.vue
+++ demo-vue/app/examples/CardViews.vue
@@ -1,39 +1,29 @@
 <template>
-  <Page>
-    <ActionBar :title="title">
-      <NavigationButton text="Back"
-                        android.systemIcon="ic_menu_back"
-                        @tap="onNavigationButtonTap"></NavigationButton>
-    </ActionBar>
-    <StackLayout class="bg-green">
-      <ScrollView>
-        <MDCardView margin="20" width="200" height="100" rippleColor="blue">
-          <Label text="Hello world" class="title"></Label>
-        </MDCardView>
-      </ScrollView>
-    </StackLayout>
-  </Page>
+    <Page>
+        <ActionBar :title="title">
+            <NavigationButton text="Back" android.systemIcon="ic_menu_back" @tap="onNavigationButtonTap"></NavigationButton>
+        </ActionBar>
+        <StackLayout class="bg-green">
+            <ScrollView>
+                <MDCardView margin="20" width="200" height="100" rippleColor="blue">
+                    <Label text="Hello world" class="title"></Label>
+                </MDCardView>
+            </ScrollView>
+        </StackLayout>
+    </Page>
 </template>
 
 <script lang="ts">
 import * as frameModule from 'tns-core-modules/ui/frame';
-import Vue from 'vue'
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
 
-export const title = 'CardViews sample';
+@Component({})
+export default class CardViews extends Vue {
+    title: 'CardViews sample';
 
-export default Vue.extend({
-    name: 'CardViews',
-
-    data() {
-        return {
-            title: title,
-        };
-    },
-
-    methods: {
-        onNavigationButtonTap() {
-            frameModule.topmost().goBack();
-        },
-    },
-});
+    onNavigationButtonTap() {
+        frameModule.topmost().goBack();
+    }
+}
 </script>
