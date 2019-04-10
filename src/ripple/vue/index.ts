e33d1c92c83d9b6b51eb6854fa8f36dd99b2fa98
Martin Guillon <martin.guillon@akylas.fr>
2019-04-10 10:00:23 +0200

    oups!

--- src/ripple/vue/index.ts
+++ src/ripple/vue/index.ts
@@ -1,12 +1,8 @@
+import { Ripple } from '../ripple';
 const RipplePlugin = {
-
-  install(Vue) {
-    Vue.registerElement(
-      'MDRipple',
-      () => require('../ripple').Ripple,
-      {}
-    );
-  }
+    install(Vue) {
+        Vue.registerElement('MDRipple', () => Ripple, {});
+    }
 };
 
 export default RipplePlugin;
