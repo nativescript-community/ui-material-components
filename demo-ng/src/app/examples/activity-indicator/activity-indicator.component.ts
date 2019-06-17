1a613b568ab3834e9a53e26a1ff198408eedb468
Henry Chavez <henry19.chavez@gmail.com>
2019-06-16 20:38:58 -0500

    feat(angular-support): add angular support for activity indicator

---
+++ demo-ng/src/app/examples/activity-indicator/activity-indicator.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'ns-activity-indicator',
+    templateUrl: './activity-indicator.component.html',
+    moduleId: module.id
+})
+export class ActivityIndicatorComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
--- src/activityindicator/angular/index.ts
+++ src/activityindicator/angular/index.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { registerElement } from 'nativescript-angular/element-registry';
 
-@NgModule()
+import { Directive } from '@angular/core';
+
+@Directive({ selector: 'MDActivityIndicator' })
+export class MaterialActivityIndicatorDirective {}
+
+@NgModule({
+    declarations: [MaterialActivityIndicatorDirective],
+    exports: [MaterialActivityIndicatorDirective]
+})
 export class NativeScriptMaterialActivityIndicatorModule {}
 
 registerElement('MDActivityIndicator', () => require('../activityindicator').ActivityIndicator);
