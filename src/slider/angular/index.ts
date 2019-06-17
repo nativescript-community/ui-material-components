736633de3f021209d2df6c5111bc29eebd2b9ce7
Henry Chavez <henry19.chavez@gmail.com>
2019-06-16 20:42:20 -0500

    feat(angular-support): add angular support for slider

--- src/slider/angular/index.ts
+++ src/slider/angular/index.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { registerElement } from 'nativescript-angular/element-registry';
 
-@NgModule()
+import { Directive } from '@angular/core';
+
+@Directive({ selector: 'MDSlider' })
+export class MaterialSliderDirective {}
+
+@NgModule({
+    declarations: [MaterialSliderDirective],
+    exports: [MaterialSliderDirective]
+})
 export class NativeScriptMaterialSliderModule {}
 
 registerElement('MDSlider', () => require('../slider').Slider);
