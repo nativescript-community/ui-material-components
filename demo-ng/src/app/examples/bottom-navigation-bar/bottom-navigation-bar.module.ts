8ef7d341dedc9ee84201965258e871e3a7f1650e
Henry Chavez <henry19.chavez@gmail.com>
2019-09-17 00:13:57 -0500

    docs: update demo-ng to include bottom navigation bar

---
+++ demo-ng/src/app/examples/bottom-navigation-bar/bottom-navigation-bar.module.ts
@@ -0,0 +1,14 @@
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NativeScriptCommonModule } from 'nativescript-angular/common';
+import { NativeScriptMaterialBottomNavigationBarModule } from 'nativescript-material-bottomnavigationbar/angular';
+
+import { BottomNavigationBarComponent } from './bottom-navigation-bar.component';
+import { FirstTabComponent } from './first-tab/first-tab.component';
+import { ThirdTabComponent } from './third-tab/third-tab.component';
+
+@NgModule({
+    imports: [NativeScriptCommonModule, NativeScriptMaterialBottomNavigationBarModule],
+    declarations: [BottomNavigationBarComponent, FirstTabComponent, ThirdTabComponent],
+    schemas: [NO_ERRORS_SCHEMA]
+})
+export class BottomNavigationBarModule {}
