62fad541436b11e9333475f3a9e0f9382ea920c8
Martin Guillon <martin.guillon@akylas.fr>
2019-10-17 11:09:00 +0200

    fix: cleanup up tsconfigs

--- src/button/button.d.ts
+++ src/button/button.d.ts
@@ -1,3 +1,10 @@
 import { ButtonBase } from './button-common';
 
+
+
+declare module 'tns-core-modules/ui/styling/style-properties' {
+    export const androidElevationProperty;
+    export const androidDynamicElevationOffsetProperty;
+}
+
 export class Button extends ButtonBase {}
