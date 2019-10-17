3acc8703fad118719c0910a49d359befa61184e6
Martin Guillon <martin.guillon@akylas.fr>
2019-10-17 16:22:17 +0200

    fix: use more css properties

--- src/progress/progress-common.ts
+++ src/progress/progress-common.ts
@@ -1,20 +1,22 @@
+import { Color } from 'tns-core-modules/color';
+import { CssProperty, Style } from 'tns-core-modules/ui/core/properties';
 import { CSSType } from 'tns-core-modules/ui/core/view';
 import { Progress as NSProgress } from 'tns-core-modules/ui/progress';
-import { Color } from 'tns-core-modules/color';
-import { Property } from 'tns-core-modules/ui/core/properties';
 
 @CSSType('MDProgress')
 export abstract class ProgressBase extends NSProgress {}
 
-export const progressColorProperty = new Property<ProgressBase, Color>({
+export const progressColorProperty = new CssProperty<Style, Color>({
+    cssName: 'progress-color',
     name: 'progressColor',
     equalityComparer: Color.equals,
     valueConverter: v => new Color(v)
 });
-export const progressBackgroundColorProperty = new Property<ProgressBase, Color>({
+export const progressBackgroundColorProperty = new CssProperty<Style, Color>({
+    cssName: 'progress-background-color',
     name: 'progressBackgroundColor',
     equalityComparer: Color.equals,
     valueConverter: v => new Color(v)
 });
-progressColorProperty.register(ProgressBase);
-progressBackgroundColorProperty.register(ProgressBase);
+progressColorProperty.register(Style);
+progressBackgroundColorProperty.register(Style);
--- src/slider/slider-common.ts
+++ src/slider/slider-common.ts
@@ -1,37 +1,40 @@
-import { CSSType } from 'tns-core-modules/ui/core/view';
-import { Color } from 'tns-core-modules/color';
-import { booleanConverter } from 'tns-core-modules/ui/core/view-base';
-import { Property } from 'tns-core-modules/ui/core/properties';
-import { Slider as NSSlider } from 'tns-core-modules/ui/slider';
 import { cssProperty } from 'nativescript-material-core/cssproperties';
+import { Color } from 'tns-core-modules/color';
+import { CssProperty, Style } from 'tns-core-modules/ui/core/properties';
+import { CSSType } from 'tns-core-modules/ui/core/view';
+import { booleanConverter } from 'tns-core-modules/ui/core/view-base';
+import { Slider as NSSlider } from 'tns-core-modules/ui/slider';
 
 @CSSType('MDSlider')
 export abstract class SliderBase extends NSSlider {
-    @cssProperty rippleColor: Color | string;
-    @cssProperty trackBackgroundColor: Color | string;
-    @cssProperty trackFillColor: Color | string;
-    @cssProperty thumbColor: Color | string;
+    @cssProperty rippleColor: Color;
+    @cssProperty trackBackgroundColor: Color;
+    @cssProperty trackFillColor: Color;
+    @cssProperty thumbColor: Color;
     @cssProperty elevation: number;
     constructor() {
         super();
     }
 }
 
-export const thumbColorProperty = new Property<SliderBase, Color>({
+export const thumbColorProperty = new CssProperty<Style, Color>({
+    cssName: 'thumb-color',
     name: 'thumbColor',
     equalityComparer: Color.equals,
     valueConverter: v => new Color(v)
 });
-thumbColorProperty.register(SliderBase);
+thumbColorProperty.register(Style);
 
-export const trackFillColorProperty = new Property<SliderBase, Color>({
+export const trackFillColorProperty = new CssProperty<Style, Color>({
+    cssName: 'track-fill-color',
     name: 'trackFillColor',
     equalityComparer: Color.equals,
     valueConverter: v => new Color(v)
 });
-trackFillColorProperty.register(SliderBase);
-export const thumbHollowAtStartProperty = new Property<SliderBase, boolean>({
+trackFillColorProperty.register(Style);
+export const thumbHollowAtStartProperty = new CssProperty<Style, boolean>({
+    cssName: 'thumb-hollow-at-start',
     name: 'thumbHollowAtStart',
     valueConverter: booleanConverter
 });
-thumbHollowAtStartProperty.register(SliderBase);
+thumbHollowAtStartProperty.register(Style);
