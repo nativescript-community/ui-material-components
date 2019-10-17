b2cffa390d54e6811e2d02ab04085a900fc70995
Martin Guillon <martin.guillon@akylas.fr>
2019-10-17 11:04:27 +0200

    test: updated demo apps

--- demo-vue/app/examples/Mixins.vue
+++ demo-vue/app/examples/Mixins.vue
@@ -4,9 +4,10 @@
             <NavigationButton text="Back" android.systemIcon="ic_menu_back" @tap="onNavigationButtonTap"></NavigationButton>
         </ActionBar>
         <StackLayout>
-            <Button margin="10" id="button1" elevation="5" rippleColor="red" borderRadius="10" fontSize="20" text="elevated button" />
+            <Button margin="10"  rippleColor="red" backgroundColor="green" borderRadius="10" fontSize="20" text="elevated button" />
+            <Button margin="10"  rippleColor="red" fontSize="20" text="elevated button" />
             <Label margin="10" horizontalAlignment="center" backgroundColor="yellow" padding="10" elevation="1" borderRadius="20" rippleColor="red" text="elevated text" />
-            <Label margin="10" horizontalAlignment="center" padding="10" dynamicElevationOffset="3" elevation="1" rippleColor="red" text="elevated text" />
+            <Label margin="10" horizontalAlignment="center" padding="10" backgroundColor="yellow" dynamicElevationOffset="3" elevation="1" rippleColor="red" text="elevated text" />
         </StackLayout>
     </Page>
 </template>
