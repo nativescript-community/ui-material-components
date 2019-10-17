4088507cc819a336450a57140cbdd595a5b5cff3
Martin Guillon <martin.guillon@akylas.fr>
2019-10-17 16:22:47 +0200

    chore: cleanup

--- src/cardview/cardview-common.ts
+++ src/cardview/cardview-common.ts
@@ -5,21 +5,7 @@ import { ContentView } from 'tns-core-modules/ui/page';
 
 @CSSType('MDCardView')
 export abstract class CardViewBase extends ContentView {
-    // protected _borderRadius: number;
-    // protected _borderColor: Color;
-    // protected _borderWidth: number;
-
     @cssProperty elevation: number;
     @cssProperty dynamicElevationOffset: number;
     @cssProperty rippleColor: Color;
-
-    // get borderRadius(): string | Length {
-    //     return this._borderRadius;
-    // }
-    // get borderWidth(): string | Length {
-    //     return this._borderWidth;
-    // }
-    // get borderColor(): Color | string {
-    //     return this._borderColor;
-    // }
 }
