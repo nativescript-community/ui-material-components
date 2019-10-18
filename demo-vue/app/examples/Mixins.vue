0b65f4a60371a9aa52c620f57351286ca3ff79df
Martin Guillon <martin.guillon@akylas.fr>
2019-10-18 10:37:27 +0200

    test: update demo app

--- demo-vue/app/examples/Mixins.vue
+++ demo-vue/app/examples/Mixins.vue
@@ -8,6 +8,9 @@
             <Button margin="10"  rippleColor="red" fontSize="20" text="elevated button" />
             <Label margin="10" horizontalAlignment="center" backgroundColor="yellow" padding="10" elevation="1" borderRadius="20" rippleColor="red" text="elevated text" />
             <Label margin="10" horizontalAlignment="center" padding="10" backgroundColor="yellow" dynamicElevationOffset="3" elevation="1" rippleColor="red" text="elevated text" />
+            <StackLayout class="cardView" height="100" orientation="horizontal" >
+                <Label width="100%" textAlignment="center" verticalAlignment="center" text="elevated text" />
+            </StackLayout>
         </StackLayout>
     </Page>
 </template>
