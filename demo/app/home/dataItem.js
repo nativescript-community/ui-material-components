5e5269175bdea05e4c7de9f513b4845738ee961a
Martin Guillon <martin.guillon@akylas.fr>
2020-06-12 12:00:52 +0200

    test: demo app fix

---
+++ demo/app/home/dataItem.js
@@ -0,0 +1,10 @@
+Object.defineProperty(exports, "__esModule", { value: true });
+var DataItem = /** @class */ (function () {
+    function DataItem(id, src) {
+        this.id = id;
+        this.src = src;
+    }
+    return DataItem;
+}());
+exports.DataItem = DataItem;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZGF0YUl0ZW0uanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJkYXRhSXRlbS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUE7SUFJSSxrQkFBWSxFQUFVLEVBQUUsR0FBVztRQUMvQixJQUFJLENBQUMsRUFBRSxHQUFHLEVBQUUsQ0FBQztRQUNiLElBQUksQ0FBQyxHQUFHLEdBQUcsR0FBRyxDQUFDO0lBQ25CLENBQUM7SUFDTCxlQUFDO0FBQUQsQ0FBQyxBQVJELElBUUM7QUFSWSw0QkFBUSIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBjbGFzcyBEYXRhSXRlbSB7XG4gICAgcHVibGljIGlkOiBudW1iZXI7XG4gICAgcHVibGljIHNyYztcblxuICAgIGNvbnN0cnVjdG9yKGlkOiBudW1iZXIsIHNyYzogc3RyaW5nKSB7XG4gICAgICAgIHRoaXMuaWQgPSBpZDtcbiAgICAgICAgdGhpcy5zcmMgPSBzcmM7XG4gICAgfVxufVxuIl19
