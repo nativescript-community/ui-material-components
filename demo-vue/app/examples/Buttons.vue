c793a22dff7a746bb25b17db5bfb7826c1275016
Martin Guillon <martin.guillon@akylas.fr>
2020-09-21 14:28:12 +0200

    test: demo app update

--- demo-vue/app/examples/Buttons.vue
+++ demo-vue/app/examples/Buttons.vue
@@ -32,13 +32,15 @@
                         <Span text=" search" fontAttributes="Bold"></Span>
                 </MDButton>
                 <MDButton id="button3" text="disabled button"  isEnabled="false" @tap="onTap" />
-                <MDButton id="button3" text="flat button \n test" variant="flat" class="bg-blue" @tap="onTap" />
-                <MDButton id="button4" text="flat disabled button" variant="flat" isEnabled="false" backgroundColor="yellow" @tap="onTap" />
-                <MDButton id="button5" text="outline button" rippleColor="red" color="black" width="200" variant="outline" @tap="onTap" />
-                <MDButton id="button6" text="disabled outline button" borderColor="red" borderRadius="2" borderWidth="2" isEnabled="false" width="200" variant="outline" backgroundColor="green" @tap="onTap" />
-                <MDButton id="button7" text="text button" width="200" color="blue" variant="text" @tap="onTap" />
-                <MDButton id="button8" text="disabled text button" isEnabled="false" width="200" variant="text" @tap="onTap" />
-                <MDButton id="button9" class="falseFAb bg-green" text="+" color="white" verticalAlign="center" backgroundColor="#53ba82" @tap="onTap" />
+                <MDButton id="button4" text="flat button \n test" variant="flat" class="bg-blue" @tap="onTap" />
+                <MDButton id="button5" text="flat transparent button \n test" rippleColor="red" variant="flat" backgroundColor="transparent" @tap="onTap" />
+                <MDButton id="button6" text="flat disabled button" variant="flat" isEnabled="false" backgroundColor="yellow" @tap="onTap" />
+                <MDButton id="button7" text="outline button" rippleColor="red" color="black" width="200" variant="outline" @tap="onTap" />
+                <MDButton id="button8" text="disabled outline button" borderColor="red" borderRadius="2" borderWidth="2" isEnabled="false" width="200" variant="outline" backgroundColor="green" @tap="onTap" />
+                <MDButton id="button9" text="text button" width="200" color="green" variant="text" @tap="onTap" />
+                <MDButton id="button10" text="text button class" width="200" class="text_class_test" variant="text" @tap="onTap" />
+                <MDButton id="button11" text="disabled text button" isEnabled="false" width="200" variant="text" @tap="onTap" />
+                <MDButton id="button12" class="falseFAb bg-green" text="+" color="white" verticalAlign="center" backgroundColor="#53ba82" @tap="onTap" />
             </StackLayout>
         </ScrollView>
     </Page>
@@ -69,3 +71,9 @@ export default Vue.extend({
     }
 });
 </script>
+<style scoped>
+.text_class_test {
+    color: red;
+    ripple-color: green;
+}
+</style>
