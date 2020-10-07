5cf2098db6394cecd31b0086c1f02fcf28d150a5
Anthony Salamanca <salamanca@edataquest.com>
2020-10-07 17:56:02 -0400

    implement helperColor property on textfield

--- src/textfield/textfield.d.ts
+++ src/textfield/textfield.d.ts
@@ -12,6 +12,7 @@ export class TextField extends NTextField {
     nativeViewProtected: any;
 
     helper: string;
+    helperColor: Color;
     maxLength: number;
     errorColor: Color;
     floating: boolean;
