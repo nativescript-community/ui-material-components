73549eb37da268f3120bc5fcbda86c3ae37ab9fa
Martin Guillon <martin.guillon@akylas.fr>
2020-11-13 13:30:19 +0100

    Update bottomnavigationbar.ios.ts

--- src/bottomnavigationbar/bottomnavigationbar.ios.ts
+++ src/bottomnavigationbar/bottomnavigationbar.ios.ts
@@ -141,7 +141,7 @@ export class BottomNavigationBar extends BottomNavigationBarBase {
         });
         this.nativeViewProtected.items = new NSArray({ array: bottomNavigationTabs });
 
-        // TODO: Should not have to need this setTimeout(), find better way.
+        // TODO: this is for he v8 runtime. Should not have to need this setTimeout(), find better way.
         setTimeout(() => {
             this.nativeViewProtected.selectedItem = this.nativeViewProtected.items[this.selectedTabIndex];
         }, 0);
