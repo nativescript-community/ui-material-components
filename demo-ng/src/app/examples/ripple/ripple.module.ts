f233890517a139550128a311b91814a01d60751f
Martin Guillon <martin.guillon@akylas.fr>
2021-02-17 17:53:07 +0100

    test: ng demo update

--- demo-ng/src/app/examples/ripple/ripple.module.ts
+++ demo-ng/src/app/examples/ripple/ripple.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NO_ERRORS_SCHEMA, NgModule } from '@angular/core';
 import { NativeScriptCommonModule } from '@nativescript/angular';
 import { NativeScriptMaterialRippleModule } from '@nativescript-community/ui-material-ripple/angular';
 import { RippleComponent } from './ripple.component';
