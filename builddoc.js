bdedfc5057df216e20abc3f3b30c1787e8a7924a
Martin Guillon <martin.guillon@akylas.fr>
2021-03-22 09:12:14 +0100

    chore: doc update

---
+++ builddoc.js
@@ -0,0 +1,54 @@
+// @ts-check
+
+const td = require('typedoc');
+const ts = require('typescript');
+const path = require('path');
+const globby = require('globby');
+
+const typedocJson = require('./typedoc');
+
+/**
+ * @param {Object} options
+ *  @param {string} options.entryPoint
+ *  @param {string} options.outDir
+ * @param {Partial<import('typedoc').TypeDocOptions>} [typeDocOptions]
+ */
+const createTypeScriptApiDocs = exports.createTypeScriptApiDocs = async ({ entryPoint, outDir }, typeDocOptions) => {
+  const app = new td.Application();
+  app.options.addReader(new td.TSConfigReader());
+    console.log('createTypeScriptApiDocs', typeDocOptions);
+    let files = await globby(['src/**/*.d.ts', '!**/page', '!**/typings', '!**/angular'], {
+        absolute: true,
+        cwd: path.join(process.cwd())
+      })
+      console.log('files', files);
+      app.bootstrap({
+    tsconfig: 'tsconfig.json',
+    ...typedocJson,
+    ...typeDocOptions,
+    entryPoints: files
+  });
+  //@ts-ignore
+  app.options.setCompilerOptions(files, {
+    esModuleInterop: true
+  })
+  const program = ts.createProgram(
+    app.options.getFileNames(),
+    app.options.getCompilerOptions()
+  );
+
+  const project = app.converter.convert(
+    app.expandInputFiles(app.options.getValue('entryPoints')),
+    program
+  );
+
+  if (project) {
+    await app.generateDocs(project, outDir);
+  } else {
+    throw new Error(`Error creating the TypeScript API docs for ${entryPoint}.`);
+  }
+};
+// app.generateDocs(project, "./docs");
+// app.generateJson(project, "./docs.json");
+
+createTypeScriptApiDocs({outDir:"./docs"});
