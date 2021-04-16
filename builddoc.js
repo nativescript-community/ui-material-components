e6087e332ce23c52205bca1029887b244f1d73c8
Martin Guillon <martin.guillon@akylas.fr>
2021-04-16 09:30:29 +0200

    chore: doc build script

--- builddoc.js
+++ builddoc.js
@@ -13,42 +13,36 @@ const typedocJson = require('./typedoc');
  *  @param {string} options.outDir
  * @param {Partial<import('typedoc').TypeDocOptions>} [typeDocOptions]
  */
-const createTypeScriptApiDocs = exports.createTypeScriptApiDocs = async ({ entryPoint, outDir }, typeDocOptions) => {
-  const app = new td.Application();
-  app.options.addReader(new td.TSConfigReader());
+const createTypeScriptApiDocs = (exports.createTypeScriptApiDocs = async ({ entryPoint, outDir }, typeDocOptions) => {
+    const app = new td.Application();
+    app.options.addReader(new td.TSConfigReader());
     console.log('createTypeScriptApiDocs', typeDocOptions);
-    let files = await globby(['src/**/*.d.ts', 'src/**/index.ts', '!**/references.d.ts', '!**/appbar', '!**/page', '!**/typings', '!**/angular', '!**/vue', '!**/react'], {
+    const files = await globby(['src/**/*.d.ts', 'src/**/index.ts', '!**/references.d.ts', '!**/appbar', '!**/page', '!**/typings', '!**/angular', '!**/vue', '!**/react'], {
         absolute: true,
         cwd: path.join(process.cwd())
-      })
-      console.log('files', files);
-      app.bootstrap({
-    tsconfig: 'tsconfig.json',
-    ...typedocJson,
-    ...typeDocOptions,
-    entryPoints: files
-  });
-  //@ts-ignore
-  app.options.setCompilerOptions(files, {
-    esModuleInterop: true
-  })
-  const program = ts.createProgram(
-    app.options.getFileNames(),
-    app.options.getCompilerOptions()
-  );
+    });
+    console.log('files', files);
+    app.bootstrap({
+        tsconfig: 'tsconfig.json',
+        ...typedocJson,
+        ...typeDocOptions,
+        entryPoints: files
+    });
+    //@ts-ignore
+    app.options.setCompilerOptions(files, {
+        esModuleInterop: true
+    });
+    const program = ts.createProgram(app.options.getFileNames(), app.options.getCompilerOptions());
 
-  const project = app.converter.convert(
-    app.expandInputFiles(app.options.getValue('entryPoints')),
-    program
-  );
+    const project = app.converter.convert(app.expandInputFiles(app.options.getValue('entryPoints')), program);
 
-  if (project) {
-    await app.generateDocs(project, outDir);
-  } else {
-    throw new Error(`Error creating the TypeScript API docs for ${entryPoint}.`);
-  }
-};
+    if (project) {
+        await app.generateDocs(project, outDir);
+    } else {
+        throw new Error(`Error creating the TypeScript API docs for ${entryPoint}.`);
+    }
+});
 // app.generateDocs(project, "./docs");
 // app.generateJson(project, "./docs.json");
 
-createTypeScriptApiDocs({outDir:"./docs"});
+createTypeScriptApiDocs({ outDir: './docs' });
