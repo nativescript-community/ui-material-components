bf6ce50b588218b61d652cee3c14ce658fa1ef48
Juan de Dios Martínez Vallejo <vallemardeveloper@gmail.com>
2021-09-19 11:16:01 +0200

    feat: add suport dismissOnBackButton

--- src/bottomsheet/bottomsheet.android.ts
+++ src/bottomsheet/bottomsheet.android.ts
@@ -51,6 +51,10 @@ export class ViewWithBottomSheet extends ViewWithBottomSheetBase {
                     },
 
                     onBackPressed(dialog: com.nativescript.material.bottomsheet.BottomSheetDialog) {
+                        if(options.dismissOnBackButton === false){
+                            return true;
+                        }
+                        
                         if (!owner) {
                             return false;
                         }
