5300bdcebb41237bace0adec3d95b23b6f2ea670
Martin Guillon <martin.guillon@akylas.fr>
2022-03-02 14:18:02 +0100

    chore: typings cleanup

--- references.d.ts
+++ references.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="./node_modules/@nativescript/core/global-types.d.ts" />
 /// <reference path="./node_modules/@nativescript/types-ios/lib/ios.d.ts" />
-/// <reference path="./node_modules/@nativescript/types-android/lib/android/android-platform-28.d.ts" />
+/// <reference path="./node_modules/@nativescript/types-android/lib/android/android-platform-29.d.ts" />
+/// <reference path="./node_modules/@nativescript/types-android/lib/android/androidx-29.d.ts" />
 /// <reference path="./node_modules/@nativescript/types-android/lib/android/common.d.ts" />
