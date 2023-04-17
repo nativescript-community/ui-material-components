73b999ad4fdb454442289d174f83a458111fba03
farfromrefuge <dev@akylas.fr>
2023-04-17 15:07:31 +0200

    test: bottomsheet innerframe test

---
+++ demo-snippets/vue/BottomSheetInnerFrame.vue
@@ -0,0 +1,30 @@
+<template>
+    <Frame>
+        <Page>
+            <StackLayout>
+                <Stacklayout id="test2" row="0" backgroundColor="red" verticalAlignment="top">
+                    <Button id="innerButtonK" @tap="onButtonTap" text="close with result k"></Button>
+                </Stacklayout>
+                <MDTextField margin="10" variant="filled" hint="Working TextView hint 🤪" />
+            </StackLayout>
+        </Page>
+    </Frame>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+    data() {
+        return {};
+    },
+    methods: {
+        onShownInBottomSheet(args) {
+            console.log('onShownInBottomSheet');
+        },
+        onButtonTap(event) {
+            this.$closeBottomSheet(event.object.id);
+        }
+    }
+});
+</script>
