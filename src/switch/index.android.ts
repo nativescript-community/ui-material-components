03b977bfc6b3f1ef89dfd39069e7a5fc281922ee
farfromrefuge <martin.guillon@akylas.fr>
2025-10-03 14:56:29 +0200

    Update index.android.ts

--- src/switch/index.android.ts
+++ src/switch/index.android.ts
@@ -1,3 +1,4 @@
+import { Color } from '@nativescript/core';
 import { SwitchBase, onBackgroundColorProperty } from './index-common';
 import { getColorStateList } from '@nativescript-community/ui-material-core/android/utils';
 export class Switch extends SwitchBase {
